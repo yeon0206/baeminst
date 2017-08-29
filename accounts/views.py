9995910a7390a63e6f416ae7e4e21fcd5315606b
Giyeon <giyeon@Giyeonui-MacBook-Air.local>
2017-08-29 11:35:37 +0900

    profile page

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
@@ -15,3 +16,8 @@ def signup(request):
         'form' : form,
     }
     return render(request, 'accounts/signup_form.html',ctx)
+
+
+@login_required
+def profile(request):
+    return render(request, 'accounts/profile.html')
