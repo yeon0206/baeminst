c9b3939df3c939c10c82d9d503b71b23433b5d13
Giyeon <giyeon@Giyeonui-MacBook-Air.local>
2017-08-29 11:53:57 +0900

    baemin setting

--- baemin/views.py
+++ baemin/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    ctx={
+
+    }
+    return render(request, 'baemin/index.html',ctx)
--- final/urls.py
+++ final/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-
+from django.shortcuts import redirect
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls')),
     url(r'^baemin/', include('baemin.urls', namespace='baemin')),
+    url(r'^$', lambda r: redirect('baemin:index'), name='root'), #최상위에서 바로 배민페이지로 가도록 구현
 ]
