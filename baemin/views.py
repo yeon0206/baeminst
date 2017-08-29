6cacba56fa8216107b1c83202977636544a26750
Giyeon <giyeon@Giyeonui-MacBook-Air.local>
2017-08-29 12:44:56 +0900

    bootstrap

--- baemin/views.py
+++ baemin/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from .models import Shop
 
 # Create your views here.
 def index(request):
     ctx={
-
+        'shop_list': Shop.objects.all(),
     }
     return render(request, 'baemin/index.html',ctx)
